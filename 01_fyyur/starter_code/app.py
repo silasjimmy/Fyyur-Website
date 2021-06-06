21fc105225d71c2b7431718e95c9ef4576bdde7d
Silas Jimmy <jimmysilas17@gmail.com>
2021-06-06 12:18:05 +0300

    refactor: Create Artist model

--- 01_fyyur/starter_code/app.py
+++ 01_fyyur/starter_code/app.py
@@ -49,23 +49,23 @@ class Venue(db.Model):
     looking_for_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(200))
 
-# class Artist(db.Model):
-#     __tablename__ = 'Artist'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)
-#     city = db.Column(db.String(120))
-#     state = db.Column(db.String(120))
-#     phone = db.Column(db.String(120))
-#     genres = db.Column(db.String(120))
-#     image_link = db.Column(db.String(500))
-#     facebook_link = db.Column(db.String(120))
-#
-#     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-#
-#     website_link = db.Column(db.String(120))
-#     looking_for_talent = db.Column(db.Boolean)
-#     seeking_description = db.Column(db.String(200))
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+    website_link = db.Column(db.String(120))
+    looking_for_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(200))
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
